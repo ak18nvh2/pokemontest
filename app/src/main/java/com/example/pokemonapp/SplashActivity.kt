5bcae1250d1ff977f4a43e37dd910a58462c353c
ak18nvh2 <hieuhaui12@gmail.com>
2021-01-12 14:50:53 +0700

    xong splash

---
+++ app/src/main/java/com/example/pokemonapp/SplashActivity.kt
@@ -0,0 +1,33 @@
+package com.example.pokemonapp
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_splash.*
+
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+        val anim = AnimationUtils.loadAnimation(applicationContext, R.anim.splash)
+        anim.setAnimationListener(object : Animation.AnimationListener {
+            override fun onAnimationStart(animation: Animation) {}
+            override fun onAnimationEnd(animation: Animation) {
+                changeActivity()
+            }
+
+            override fun onAnimationRepeat(animation: Animation) {}
+        })
+        img_logo.startAnimation(anim)
+    }
+
+    private fun changeActivity() {
+        val intent = Intent(this@SplashActivity, HomeActivity::class.java)
+        startActivity(intent)
+        this.finish()
+    }
+}
